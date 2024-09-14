877598eccd123955f4b00b5557a7e4363a7cb567
DanilZmievskiy <danil.zmievskiy@gmail.com>
2024-09-14 12:16:51 +0300

    Utils for the conncetion to Google Cloud

---
+++ dags/utils/google_connect.py
@@ -0,0 +1,24 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+class CloudConnection:
+    """
+    CloudConnection class is used to connect to google cloud services.
+    
+    """
+    def __init__(self, key_file: str) -> None:
+        """
+        Initialize the CloudConnection object.
+        
+        Args:
+            key_file (str): path to the key file
+        """
+        self.credentials = Credentials.from_service_account_file(
+            key_file,
+            scopes=[
+                "https://www.googleapis.com/auth/spreadsheets",
+                "https://www.googleapis.com/auth/drive",
+            ],
+        )
+        self.client = gspread.authorize(self.credentials)
